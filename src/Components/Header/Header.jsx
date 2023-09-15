a081c4eafdd135f846d1b2d0bf409962cac4d39b
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-09-15 17:49:05 +0600

    header part done

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="container mx-auto mt-14 mb-8">
+      <h1 className="text-3xl text-[#1C1B1B] text-center font-bold">
+        Course Registration
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
